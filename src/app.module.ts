e9a1c30d9e2798009e64982a6cf17c690546e956
Maxence Lequeux <maxenceleq@gmail.com>
2023-02-28 14:28:10 +0100

    DB config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
+const defaultDbConf = {
+    host: 'localhost',
+    port: 5432,
+    username: 'user',
+    password: 'pass',
+    database: 'justicket'
+}
+
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+        type: 'postgres',
+        host: process.env.DB_HOST || defaultDbConf.host,
+        port: parseInt(process.env.DB_PORT) || defaultDbConf.port,
+        username: process.env.DB_USER || defaultDbConf.username,
+        password: process.env.DB_PASSWORD || defaultDbConf.password,
+        database: process.env.DB_NAME || defaultDbConf.database,
+        entities: [],
+        synchronize: process.env.DB_SYNC === 'true',
+      }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
