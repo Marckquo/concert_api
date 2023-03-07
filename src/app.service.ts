8b2d4e609fe94ba50f11cd4bfa42a013060dc940
nicolas.henaff.etu <nicolas.henaff.etu@univ-lille.fr>
2023-03-07 17:34:09 +0100

    Ajout validation + vérification signature

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,29 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { TezosToolkit } from '@taquito/taquito';
-import { validateSignature } from '@taquito/utils';
 @Injectable()
 export class AppService {
-  async getBalance(address: string) {
-    const tezos = new TezosToolkit('https://api.tez.ie/rpc/mainnet');
-    const balance = await tezos.tz.getBalance(address);
-    return balance.toNumber() / 1000000;
-  }
-
-  verifySignature(signature: string): boolean {
-    const response = validateSignature(signature);
-    // 0: invalid signature
-    // 1: invalid public key
-    // 2: invalid message
-    // 3: valid signature
-    switch (response) {
-      case 3:
-        return true;
-      case 0:
-      case 1:
-      case 2:
-        return false;
-      default:
-        return false;
-    }
-  }
 }
--- src/tezos/tezos.service.ts
+++ src/tezos/tezos.service.ts
@@ -1,4 +1,30 @@
 import { Injectable } from '@nestjs/common';
+import { TezosToolkit } from '@taquito/taquito';
+import { validateSignature, verifySignature } from '@taquito/utils';
 
 @Injectable()
-export class TezosService {}
+export class TezosService {
+    async getBalance(address: string) {
+        const tezos = new TezosToolkit('https://api.tez.ie/rpc/mainnet');
+        const balance = await tezos.tz.getBalance(address);
+        return balance.toNumber() / 1000000;
+    }
+
+    verifySignature(signature: string, message: string, publicKey: string): boolean {
+        const response = validateSignature(signature);
+        // 0: invalid signature
+        // 1: invalid public key
+        // 2: invalid message
+        // 3: valid signature
+        switch (response) {
+            case 3:
+                return verifySignature(signature, message, publicKey);
+            case 0:
+            case 1:
+            case 2:
+                return false;
+            default:
+                return false;
+        }
+    }
+}
