af44fe061f61cf5b9cf75852b1474d6b93d62d35
Maxence Lequeux <maxenceleq@gmail.com>
2023-03-28 21:24:09 +0200

    fixed axios

--- src/metadata/metadata.service.ts
+++ src/metadata/metadata.service.ts
@@ -1,24 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import axios from 'axios';
 
 const PINATA_API_KEY = "08df48d3077bbc35fcbc";
 const PINATA_SECRET_API_KEY = "8517409ccd0bec03944706701d4ed721c96a2ab81411840d571c1ace6c7d1d42"
 
 @Injectable()
 export class MetadataService {
-
-    async sendRequestToPinata(url: string, method: string, content: string) {
-        var axios = require('axios');
-
-        var config = {
+    async sendRequestToPinata(url: string, method: string, content: string): Promise<any> {
+        const config = {
         method: method,
         url: url,
-        headers: { 
+        headers: {
                 'pinata_api_key':  PINATA_API_KEY,
                 'pinata_secret_api_key': PINATA_SECRET_API_KEY
             }
         };
 
-        const res = await axios(config)
+        return axios(config);
     }
-    
 }
