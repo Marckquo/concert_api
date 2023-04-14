63a7f4a0d02a23a9acfcdaf40219e5639c4be439
NicolasHenaff <nicolas.henaf.etu@univ-lille.fr>
2023-04-14 13:55:47 +0000

    Ajout test admin.controller

--- src/admin/admin.controller.spec.ts
+++ src/admin/admin.controller.spec.ts
@@ -1,18 +1,47 @@
+import { Reflector } from '@nestjs/core';
 import { Test, TestingModule } from '@nestjs/testing';
+import { BearerGuard } from 'src/auth/bearer.guard';
 import { AdminController } from './admin.controller';
+import { AdminService } from './admin.service';
+
+jest.mock('./admin.service');
 
 describe('AdminController', () => {
-  let controller: AdminController;
+    let controller: AdminController;
+    let adminService: AdminService;
 
-  beforeEach(async () => {
-    const module: TestingModule = await Test.createTestingModule({
-      controllers: [AdminController],
-    }).compile();
+    beforeEach(async () => {
+        const module: TestingModule = await Test.createTestingModule({
+            controllers: [AdminController],
+            providers: [AdminService, Reflector],
+        })
+        .overrideGuard(BearerGuard)
+        .useValue({})
+        .compile();
 
-    controller = module.get<AdminController>(AdminController);
-  });
+        controller = module.get<AdminController>(AdminController);
+        adminService = module.get<AdminService>(AdminService);
+    });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
-  });
+    afterEach(() => {
+        jest.clearAllMocks();
+    });
+
+    it('should be defined', () => {
+        expect(controller).toBeDefined();
+    });
+
+    describe('isAdmin', () => {
+        it('should return isAdmin status', async () => {
+            const walletAddress = 'tz1abcdefgh1234567890';
+            const request = { walletAddress };
+            const expectedResult = { isAdmin: true };
+
+            jest.spyOn(adminService, 'isAdmin').mockResolvedValue(true);
+
+            const result = await controller.isAdmin(request);
+            expect(result).toEqual(expectedResult);
+            expect(adminService.isAdmin).toHaveBeenCalledWith(walletAddress);
+        });
+    });
 });
